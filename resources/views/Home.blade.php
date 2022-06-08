6647957034e120e84f08e67fc928e991d6c05a79
Rosario Garufi <saro.garufi1@gmail.com>
2022-06-08 15:13:20 +0200

    Consegna esercizio con bonus

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+
+            
+            html, body {
+                background-color: #fff;
+                color: black;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .container{
+                
+                
+            }
+            header{
+                padding:20px;
+                height: 10vh;
+                display:flex;
+                justify-content:space-between;
+            }
+            header .menu{
+                display:flex;
+                justify-items:center;
+                justify-content:center;
+                align-items:center;
+                list-style:none;
+            }
+
+            li{
+                margin:15px;
+                
+            }
+
+            h1{
+                text-transform:uppercase;
+                color:red;
+            }
+            a{
+                text-decoration:none;
+                color:black;
+                font-size: 18px;
+                letter-spacing:1px;
+            }
+
+            a:hover{
+                color:red;
+            }
+
+            h2{
+                text-align:center;
+            }
+
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <header>
+                <h1>Welcome To Laravel</h1>
+                <nav>
+                <ul class="menu">
+                    @foreach($links as $link)
+                    <li>
+                        <a href="/Home">{{$link}}</a>
+                    </li>
+
+                    @endforeach
+                </ul>
+                </nav>
+            </header>
+        </div>
+        <h2>{{$hello}}</h2>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,32 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+
+A questo punto, iniziamo a prendere confidenza con le rotte e le views:
+cancelliamo la view welcome.blade.php e creiamo una nostra homepage.
+Facciamo quindi sì che la rotta / visualizzi home.blade.php
+Inizialmente stampiamo un Hello World, poi passiamo dei dati alla view in modo da visualizzarli dinamicamente con Blade.
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::get('/Home', function(){
+    $hello = 'Ciao Laravel :)';
+    $links = ['Home', 'Abaut', 'Contact'];
+    return view('Home', compact('hello', 'links'));
+    
+});
+
+Route::get('/Abaut', function(){
+    $hello = 'Laravel è un framework PHP per applicazioni web. Rilasciato 
+    nel 2011 come framework open source, Laravel si basa sul pattern MVC e
+     presenta una sintassi espressiva ed elegante.Laravel semplifica alcune
+      delle attività più comuni dei progetti web, come autenticazione,
+       autorizzazione, routing, sessioni, caching. Possiede inoltre un 
+       motore di templating integrato, Blade, un sistema di migrazione 
+       database, integrazione con PHPUnit per il testing, e una propria
+        interfaccia a riga di comando, chiamata Artisan CLI.';
+    $links = ['Home', 'Abaut', 'Contact'];
+    return view('Home', compact('hello', 'links'));
+    
 });
